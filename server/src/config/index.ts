642932341dec7d74497394e8dd85157d14f6fb1f
TheMathys <mathysbesnard.mb@gmail.com>
2023-04-13 16:09:35 +0200

    Add some configuration parameters.

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,2 +1,5 @@
+import * as process from "process";
+
 export const {DB_HOST, DB_NAME, DB_PORT, DB_USERNAME, DB_PASSWORD} = process.env;
 export const {JWT_SECRET} = process.env;
+export const {COOKIE_SECRET} = process.env;
--- server/src/config/mikro-orm.config.ts
+++ server/src/config/mikro-orm.config.ts
@@ -1,13 +1,14 @@
 import {defineConfig} from "@mikro-orm/postgresql";
 import {DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME} from "@/config";
 import {User} from "@/entity";
-
 export default defineConfig({
     clientUrl: `postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+    type: "postgresql",
     tsNode: true,
     debug: true, // à changer
     forceUtcTimezone: true,
     entities: [
         User,
-    ]
+    ],
+    allowGlobalContext: true
 });
